19e17d5059a83da4ba5443fc25fb3b55b063cea1
caetanojpo <106156212+caetanojpo@users.noreply.github.com>
2024-03-19 13:01:15 -0300

    DTO validations

--- vib-user/src/main/java/br/com/valeimoveisadm/vibuser/adapter/inbound/rest/dto/request/ChangeAddress.java
+++ vib-user/src/main/java/br/com/valeimoveisadm/vibuser/adapter/inbound/rest/dto/request/ChangeAddress.java
@@ -1,15 +1,34 @@
 package br.com.valeimoveisadm.vibuser.adapter.inbound.rest.dto.request;
 
 import br.com.valeimoveisadm.vibuser.application.core.enums.States;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Positive;
 
 public record ChangeAddress(
+        @NotBlank(message = "Street is required")
         String street,
+
+        @NotNull(message = "Number is required")
+        @Positive(message = "Number must be positive")
         Integer number,
+
         String complement,
+
+        @NotBlank(message = "Neighborhood is required")
         String neighborhood,
+
+        @NotBlank(message = "City is required")
         String city,
+
+        @Pattern(regexp = "\\d{5}-\\d{3}", message = "Invalid postal code format (XXXXX-XXX)")
         String postalCode,
+
+        @NotNull(message = "State is required")
         States state,
+
+        @NotBlank(message = "Country is required")
         String country
 ) {
 }
--- vib-user/src/main/java/br/com/valeimoveisadm/vibuser/adapter/inbound/rest/dto/request/CreateUser.java
+++ vib-user/src/main/java/br/com/valeimoveisadm/vibuser/adapter/inbound/rest/dto/request/CreateUser.java
@@ -2,10 +2,7 @@ package br.com.valeimoveisadm.vibuser.adapter.inbound.rest.dto.request;
 
 import br.com.valeimoveisadm.vibuser.application.core.domain.Address;
 import br.com.valeimoveisadm.vibuser.application.core.enums.UserPermission;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 
 public record CreateUser(
         @NotBlank(message = "{NOT.BLANK}")
@@ -15,20 +12,33 @@ public record CreateUser(
         @NotBlank(message = "{NOT.BLANK}")
         @Pattern(
                 regexp = "^(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$",
-                message = "A Senha deve ter pelo menos 8 caracteres de comprimento, conter pelo menos uma letra maiúscula, um dígito e um caractere especial."
+                message = "The password must be at least 8 characters long, contain at least one uppercase letter, one digit, and one special character."
         )
         String password,
 
         @NotBlank
+        @Size(min = 2, max = 100, message = "Must have between 2 to 100 characters")
         String name,
 
         @NotBlank
+        @Pattern(
+                regexp = "^\\d{3}\\.\\d{3}\\.\\d{3}-\\d{2}$",
+                message = "Please enter a valid CPF number in the format XXX.XXX.XXX-XX, where each X represents a digit. For example: 123.456.789-00."
+        )
         String cpf,
 
         @NotBlank
+        @Pattern(
+                regexp = "^\\d{2}\\.\\d{3}\\.\\d{3}-\\d$",
+                message = "Please enter a valid RG number in the format XX.XXX.XXX-X, where each X represents a digit. For example: 12.345.678-9."
+        )
         String rg,
 
         @NotBlank
+        @Pattern(
+                regexp = "^\\+\\d{1,3}\\s?\\(\\d{1,3}\\)\\s?\\d{3,4}-?\\d{4}$",
+                message = "Please enter a valid cellphone number. Cellphone numbers should be in the format +XX-XXX-XXX-XXXX or XXX-XXX-XXXX and may include spaces, dashes, or periods. Make sure you have entered the correct number and try again."
+        )
         String cellphone,
 
         @NotNull
